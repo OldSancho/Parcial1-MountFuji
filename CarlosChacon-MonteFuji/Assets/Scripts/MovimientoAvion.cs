29ab2fc2ea2c98820800e13fd808de5a70da0ef8
Carlos Chacon Lozano <theoldsancho@gmail.com>
2018-03-01 03:26:12 -0500

    Parcial1-MountFuji

---
+++ CarlosChacon-MonteFuji/Assets/Scripts/MovimientoAvion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoAvion : MonoBehaviour
+{
+
+    public float speed = 50f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(0, 0, speed * Time.deltaTime);
+
+    }
+}
